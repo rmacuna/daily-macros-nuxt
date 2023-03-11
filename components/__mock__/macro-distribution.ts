622812bf56f029206eb98a5ac0ab223f9291f168
Roberto acuña <robertoacunapolo@gmail.com>
2023-03-10 19:15:38 -0500

    feat: add macro-distribution mock data

---
+++ components/__mock__/macro-distribution.ts
@@ -0,0 +1,19 @@
+const macroDistribution = [
+  {
+    macro: "carbs",
+    qty: 50,
+    max: 250,
+  },
+  {
+    macro: "protein",
+    qty: 70,
+    max: 140,
+  },
+  {
+    macro: "fat",
+    qty: 60,
+    max: 90,
+  }
+]
+
+export default macroDistribution;
