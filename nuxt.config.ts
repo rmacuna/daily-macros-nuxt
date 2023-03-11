2acf682a8d74bc9d3cf3e202003faa53dbda0fe0
Roberto acuña <robertoacunapolo@gmail.com>
2023-03-10 19:14:58 -0500

    feat: update gitignore

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   typescript: {
-    shim: false
+    shim: false,
   },
-  modules: ['@nuxtjs/tailwindcss']
-})
+  modules: ["@nuxtjs/tailwindcss"]
+});
