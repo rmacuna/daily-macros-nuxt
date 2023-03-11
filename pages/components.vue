9a400b9fc3d0203b0313bbdcb5abb5e7280aee0e
Roberto acuña <robertoacunapolo@gmail.com>
2023-03-10 19:15:08 -0500

    feat: create index page

---
+++ pages/components.vue
@@ -0,0 +1,23 @@
+
+<script setup lang="ts">
+import macroDistributionMock from '~~/components/__mock__/macro-distribution';
+
+interface MacroGoal {
+  macro: 'carbs' | 'protein' | 'fat';
+  qty: number;
+  max: number;
+}
+const dailyDistribution = ref(macroDistributionMock as MacroGoal[]);
+
+</script>
+
+<template>
+  <div class="max-w-7xl mx-auto">
+
+    <!-- Macros Card -->
+    <div class="py-4">
+      <h1 class="text-sm font-bold text-slate-500">Macro Card</h1>
+      <MacrosCard :daily-distribution="dailyDistribution" />
+    </div>
+  </div>
+</template>
