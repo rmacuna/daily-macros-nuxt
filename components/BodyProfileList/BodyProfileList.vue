ef3703d8012a383d7058f77de7d3b4e5541f2a95
Roberto acuña <robertoacunapolo@gmail.com>
2023-03-11 12:15:25 -0500

    feat: add body-profile-list component

---
+++ components/BodyProfileList/BodyProfileList.vue
@@ -0,0 +1,10 @@
+<template>
+  <ul>
+    <ListItem v-for="{ title, value } of bodyProfileList" :title="title" :description="value" separators />
+  </ul>
+</template>
+
+<script setup>
+  defineProps(['bodyProfileList']);
+
+</script>
