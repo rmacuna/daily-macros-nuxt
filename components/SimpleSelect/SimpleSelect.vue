f9c9d049a7e184e3a9c2c5838b61c449991c2c31
Roberto acuña <robertoacunapolo@gmail.com>
2023-03-11 12:14:07 -0500

    feat: add simple select component

---
+++ components/SimpleSelect/SimpleSelect.vue
@@ -0,0 +1,16 @@
+<template>
+  <select :name="name" :id="id">
+    <option v-for="option in data" :key="option.value" :value="option.value">{{ option.label }}</option>
+  </select>
+</template>
+
+<script setup lang="ts">
+
+interface Props {
+  name: string;
+  id: string;
+  data: Record<string, string | number>[];
+}
+
+defineProps<Props>();
+</script>
