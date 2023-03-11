18790a538df0b8e10be7486672b0b0a41b546094
Roberto acuña <robertoacunapolo@gmail.com>
2023-03-11 12:15:11 -0500

    feat: add program selection component

---
+++ components/ProgramSelection/ProgramSelection.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1 class="text-[40px] pb-1 font-semibold text-slate-900 leading-[30px]">
+    <span class="text-2xl text-slate-600">{{ name }}'s</span> <br /> Program
+  </h1>
+
+  <SimpleSelect class="mt-2" name="program-selection" id="program-selection" :data="basicPrograms" />
+</template>
+
+<script setup lang="ts">
+import basicPrograms from '../constants/basic-programs';
+
+interface Props {
+  name: string;
+}
+defineProps<Props>()
+</script>
