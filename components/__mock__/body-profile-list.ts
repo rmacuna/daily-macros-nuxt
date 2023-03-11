9d782c942f4e43b94c32e0a78764ccfbea7c828a
Roberto acuña <robertoacunapolo@gmail.com>
2023-03-11 12:14:48 -0500

    feat: add mock data for body-profile-list

---
+++ components/__mock__/body-profile-list.ts
@@ -0,0 +1,34 @@
+const bodyProfileList = [
+  {
+    icon: '',
+    title: 'Basal metabolism',
+    value: '1503 kcal',
+  },
+  {
+    icon: '',
+    title: 'BMI',
+    value: '23.42',
+  },
+  {
+    icon: '',
+    title: 'Body fat %',
+    value: '10.1',
+  },
+  {
+    icon: '',
+    title: 'Water requirements (ml)',
+    value: '10.1',
+  },
+  {
+    icon: '',
+    title: 'Height (cm)',
+    value: '161cm',
+  },
+  {
+    icon: '',
+    title: 'Weight (kg)',
+    value: '67.7',
+  }
+]
+
+export default bodyProfileList;
---
+++ components/constants/basic-programs.ts
@@ -0,0 +1,24 @@
+const basicPrograms = [
+  {
+    label: 'Lose weight',
+    value: 'lose_weight'
+  },
+  {
+    label: 'Lose weight slowly',
+    value: 'lose_weight_soft'
+  },
+  {
+    label: 'Maintain weight',
+    value: 'maintainance'
+  },
+  {
+    label: 'Gain Weight',
+    value: 'gain_weight'
+  },
+  {
+    label: 'Gain weight slowly',
+    value: 'gain_weight_soft'
+  }
+]
+
+export default basicPrograms;
